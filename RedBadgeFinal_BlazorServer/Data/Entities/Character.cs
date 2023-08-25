0bef8cde3b86647fb7b84ff8cbda2e5d7cd17ae8
Clarissa Fields <clarissa.alaze@gmail.com>
2023-08-24 21:05:47 -0400

    Initial Commit with project files and .gitignore

---
+++ RedBadgeFinal_BlazorServer/Data/Entities/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace RedBadgeFinal_BlazorServer.Data.Entities
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Region> Regions { get; set; } = new List<Region>();
+
+    }
+}
---
+++ RedBadgeFinal_BlazorServer/Data/Entities/Element.cs
@@ -0,0 +1,6 @@
+﻿namespace RedBadgeFinal_BlazorServer.Data.Entities
+{
+    public class Element
+    {
+    }
+}
